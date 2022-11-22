6e6d3720842ef3a7fde62eb741ae40ffc450b89b
Axel <lucasaxelqg15@gmail.com>
2022-11-21 23:17:26 -0300

    todo ok

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const section = document.getElementById("section")
 const button = document.getElementById("button");
-const imgCont = document.getElementById("container");
-const pokeInfo = document.getElementById("poke-information")
+const imgCont = document.getElementById("front");
+const pokeInfo = document.getElementById("back")
 const pokeInput = document.getElementById("input") 
+const xButton = document.getElementById("x-button")
+const infoUser = document.getElementById("info-user")
 
+xButton.addEventListener("click",()=>infoUser.style.display="none")
 
 const pokeSet =(data)=>{
   let info = ""
   let name = data.name.toUpperCase()
   info += `
   <h2 id="name-h2">${name}</h2>
-  <h2 id="type-h2">Tipo: ${data.types[0].type.name}</h2>
+  <h2 id="id-h2">ID#: ${data.id}</h2>
+  <h2 id="type-h2">Tipo: ${data.types.map((tipo) =>`<span class="${tipo.type.name}">${tipo.type.name.toUpperCase()}</span>`).join(" ")}</h2>
   <h2 id="height-h2">Altura: ${data.height*10} Cm</h2>
   <h2 id="weight-h2">Peso: ${data.weight/10} Kg</h2>
   `
@@ -23,6 +27,7 @@ const addImg = (call)=>{
               <img id="poke-img" src=${call.sprites.other.home.front_default} alt="pokeimg">
             </div>`
   imgCont.innerHTML=pokeImg
+  pokeInfo.style.background="radial-gradient(circle, rgba(172,126,162,1) 0%, rgba(42,40,159,1) 49%, rgba(68,63,66,1) 92%)"
 }
 
 const showError =(text)=>{
@@ -44,7 +49,7 @@ const valid = ()=>{
   let cont = pokeInput.value
   console.log(cont);
   if(cont=="")showError("Por favor ingrese un numero")
-  else if(cont<900&&cont>0)callApi()
+  else if(cont<899&&cont>0)callApi()
   else(showError("No se encontro ningun pokemon"))
 }
 
